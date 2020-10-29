dfff2c883d82dfcea650fdf04cf1d63b0719b252
Rain7_osu <1789446861@qq.com>
2020-10-30 00:00:06 +0800

    first init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        bsss，blog for students of software school
+      </header>
+    </div>
+  );
+}
+
+export default App;
