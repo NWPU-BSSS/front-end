2b90ea12bd2ed2ee24e0433176b624b66ae2b4c3
WHuan17321 <57995792+WHuan17321@users.noreply.github.com>
2020-11-06 19:50:05 +0800

    20201106-test

--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import {Button, Layout} from "antd";
 
 export class TestPage extends Component {
 
   render () {
     return (
       <div>
-
+        <Layout>
+          <Button/>
+        </Layout>
       </div>
     )
   }
