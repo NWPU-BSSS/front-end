231a58b5561bf435b4bcf78a3f417e6b420f80e7
Rain7_osu <1789446861@qq.com>
2020-11-18 22:02:21 +0800

    【feat】实现了markdown 在线编辑

--- src/components/release-blog-components/MdEditor.js
+++ src/components/release-blog-components/MdEditor.js
@@ -1,10 +1,68 @@
 import React from 'react'
 import './ReleaseBlogComponents.css'
+import ReactMarkdown from 'react-markdown'
+import './github-markdown.min.css'
 
-export function MdEditor (props) {
-  return (
-    <div className="MdEditor">
+export class MdEditor extends React.Component {
 
-    </div>
-  )
+  constructor (props) {
+    super(props)
+    this.state = {
+      markdown: '',
+      textareaHeight: 500
+    }
+  }
+
+  componentDidMount () {
+    window.addEventListener('resize', this.handleResize)
+  }
+
+  handleResize = e => {
+
+  }
+
+  handleInputMarkdown = e => {
+    let previewHeight = this.preview.scrollHeight
+    let textareaHeight = e.target.scrollHeight
+    let height = Math.max(previewHeight, textareaHeight, 500)
+    this.setState({
+      markdown: e.target.value,
+      textareaHeight: height
+    })
+  }
+
+  handleKeyPressMarkdown = e => {
+    if (e.keyCode === 9) {
+      e.preventDefault()
+      this.setState({
+        markdown: this.state.markdown + '\t'
+      })
+    } else if (e.keyCode === 13) return false
+    else if(e.keyCode === 17) {
+      let previewHeight = this.preview.scrollHeight
+      let textareaHeight = e.target.scrollHeight
+      let height = Math.max(previewHeight, textareaHeight, 500)
+      this.setState({
+        markdown: e.target.value,
+        textareaHeight: height
+      })
+    }
+  }
+
+  render () {
+
+    return (
+      <div className="MdEditor" style={{
+        height: this.state.textareaHeight
+      }}>
+        <div className="markdown-input">
+          <textarea onKeyDown={this.handleKeyPressMarkdown} onChange={this.handleInputMarkdown}
+                    value={this.state.markdown}/>
+        </div>
+        <div ref={div => this.preview = div} className="markdown-preview">
+          <ReactMarkdown source={this.state.markdown} className="markdown-body"/>
+        </div>
+      </div>
+    )
+  }
 }
