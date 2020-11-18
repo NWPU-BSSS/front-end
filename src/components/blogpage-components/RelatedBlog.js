9c307dbc3c5419f2843e05ce2f08dc7ec3551468
Nreyab <51963659+Nreyab@users.noreply.github.com>
2020-11-18 20:59:57 +0800

    【feat】实现博客页面下方相关博客推荐列表组件

---
+++ src/components/blogpage-components/RelatedBlog.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import './RelatedBlog.css'
+import viewLogo from '../../assets/img/u583.png'
+
+export class RelatedBlog extends Component {
+
+    render() {
+        return (
+            <div className={'related-blog'}>
+                <div className={'related-head'}>
+                    <div className={'related-head-title'}>
+                        <a href={''}>Blog title</a>
+                    </div>
+                    <div className={'related-head-author'}>
+                        <a href={''}>UserX的博客</a>
+                    </div>
+                    <div className={'related-head-view'}>
+                        <img src={viewLogo} alt={'logo'}/>
+                        <div>
+                            8848
+                        </div>
+                    </div>
+                </div>
+                <div className={'related-content'}>
+                    content
+                </div>
+            </div>
+        )
+    }
+
+}
