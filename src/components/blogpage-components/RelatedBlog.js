315cf88b79093bc3493e34043e7c15ad50cb70b3
Nreyab <51963659+Nreyab@users.noreply.github.com>
2020-11-19 00:38:22 +0800

    【style】纠正代码格式

--- src/components/blogpage-components/RelatedBlog.js
+++ src/components/blogpage-components/RelatedBlog.js
@@ -4,28 +4,28 @@ import viewLogo from '../../assets/img/u583.png'
 
 export class RelatedBlog extends Component {
 
-    render() {
-        return (
-            <div className={'related-blog'}>
-                <div className={'related-head'}>
-                    <div className={'related-head-title'}>
-                        <a href={''}>Blog title</a>
-                    </div>
-                    <div className={'related-head-author'}>
-                        <a href={''}>UserX的博客</a>
-                    </div>
-                    <div className={'related-head-view'}>
-                        <img src={viewLogo} alt={'logo'}/>
-                        <div>
-                            8848
-                        </div>
-                    </div>
-                </div>
-                <div className={'related-content'}>
-                    content
-                </div>
+  render () {
+    return (
+      <div className={'related-blog'}>
+        <div className={'related-head'}>
+          <div className={'related-head-title'}>
+            <a href={''}>Blog title</a>
+          </div>
+          <div className={'related-head-author'}>
+            <a href={''}>UserX的博客</a>
+          </div>
+          <div className={'related-head-view'}>
+            <img src={viewLogo} alt={'logo'}/>
+            <div>
+              8848
             </div>
-        )
-    }
+          </div>
+        </div>
+        <div className={'related-content'}>
+          content
+        </div>
+      </div>
+    )
+  }
 
 }
