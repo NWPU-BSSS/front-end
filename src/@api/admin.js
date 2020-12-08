1a352b521e8dfd39498f4d8c3387875f90688b4e
张一夫 <randyzhang@mail.nwpu.edu.cn>
2020-12-08 09:53:33 +0800

    【feat】 finish admin api(except delete function)

---
+++ src/@api/admin.js
@@ -0,0 +1,48 @@
+import {getInit, postInit, query, request} from './@config'
+
+//
+/**
+ //  * 删除某个博客
+ //  * @param {string}admin
+ //  * @param {string}password
+ //  * @param {string}blogId
+ //  * @returns {Promise<JSON>}
+ //  */
+// export const deleteBlog = async({admin, password, blogId}) =>
+//
+// /**
+//  * 删掉用户（删库）
+//  * @param {string}admin
+//  * @param {string}userId
+//  * @param {string}password
+//  * @returns {Promise<JSON>}
+//  */
+// export const deleteUser = async({admin, userId, password}) =>
+
+/**
+ * 发布公告
+ * @param {string}admin
+ * @param {string}password
+ * @param {string}content
+ * @param {string}title
+ * @param {string}endTime
+ * @param {string}startTime
+ * @returns {Promise<*>}
+ */
+export const releaseAnnouncement = async ({admin, password, content, title, endTime, startTime}) =>
+    request('/api/admin/announcement', postInit({admin, password, content, title, endTime, startTime}))
+
+/**
+ * 获取所有博客列表
+ * @returns {Promise<JSON>}
+ */
+export const getBlogList = async () =>
+    request('/api/admin/blogs', getInit())
+
+/**
+ * 获取用户列表
+ * @returns {Promise<JSON>}
+ */
+export const getUserList = async () =>
+    request('api/admin/users')
+
