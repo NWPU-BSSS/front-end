78e70c86db7306cb529a140428e4c14ea1987759
Riollee <530377286@qq.com>
2020-12-11 13:12:22 +0800

    【feat】 merge AdminBlogPage

--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react'
 import { login } from '../@api'
-import { BlogTable } from '../components/manager-blogs-components/BlogTable'
+import { BlogTable } from '../components/admin-blog-components/AdminBlogTable'
+import { AdminBlogSearch } from '../components/admin-blog-components/AdminBlogSearch'
 
 export class TestPage extends Component {
-
-  handleClick = async () => {
-    await login({ email: '925993394@qq.com', password: '0' })
-  }
 
   render () {
     return (
       <div>
-        <button onClick={this.handleClick}>点击</button>
-        <BlogTable />
+        <AdminBlogSearch/>
+        <BlogTable/>
       </div>
     )
   }
+
+  // handleClick = async () => {
+  //   await login({ email: '925993394@qq.com', password: '0' })
+  // }
+  //
+  // render () {
+  //   return (
+  //     <div>
+  //       <button onClick={this.handleClick}>点击</button>
+  //     </div>
+  //   )
+  // }
+
+
 }
