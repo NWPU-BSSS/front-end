e613ff13314fd744a4bd0da0e4e28d35125465ed
45011 <450116859@qq.com>
2020-12-11 16:20:29 +0800

    【feat】add AdminUsersPage中顶部查询界面

--- src/pages/AdminUsersPage.js
+++ src/pages/AdminUsersPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 // import { connect } from 'react-redux'
 // import { getAdminUsersAsync } from '../@redux/actions_async'
+import './AdminUsersPage.css'
 import { AdminUsersTable } from '../components/admin-user-components/AdminUsersTable'
 
 class AdminUsersPage extends Component {
@@ -8,6 +9,21 @@ class AdminUsersPage extends Component {
   render () {
     return (
       <div className="AdminUserPage">
+        <div className="ManageUsers">Manage Users</div>
+        <div className="UserName">Username:</div>
+        <input className="InputUserName"></input>
+        <div className="NickName">Nickname:</div>
+        <input className="InputNickName"></input>
+        <button className="Query">Query</button>
+        <button className="Reset">Reset</button>
+        <button className="NewUser">+ New User</button>
+        <button className="BulkOperation">Bulk Operation</button>
+        <button className="More">...</button>
+        <div className="SelectedMessage">
+          <button className="SelectedNumber">4</button>
+          <div className="Font"> items have been selected</div>
+          <button className="Clear">clear</button>
+        </div>
         <h1>admin user</h1>
         <AdminUsersTable/>
       </div>
