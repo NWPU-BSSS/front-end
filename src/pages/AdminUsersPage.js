c4413aa5e3de3e85815c871e0d7f2f1c4d174288
Riollee <530377286@qq.com>
2020-12-12 00:55:00 +0800

    【feat】 merge adminUser page

--- src/pages/AdminUsersPage.js
+++ src/pages/AdminUsersPage.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react'
 // import { connect } from 'react-redux'
 // import { getAdminUsersAsync } from '../@redux/actions_async'
-import './AdminUsersPage.css'
 import { AdminUsersTable } from '../components/admin-user-components/AdminUsersTable'
+import { AdminUsersSearch } from '../components/admin-user-components/AdminUsersSearch'
 
 class AdminUsersPage extends Component {
 
   render () {
     return (
-      <div className="AdminUserPage">
-        <div className="ManageUsers">Manage Users</div>
-        <div className="UserName">Username:</div>
-        <input className="InputUserName"/>
-        <div className="NickName">Nickname:</div>
-        <input className="InputNickName"/>
-        <button className="Query">Query</button>
-        <button className="Reset">Reset</button>
-        <button className="NewUser">+ New User</button>
-        <button className="BulkOperation">Bulk Operation</button>
-        <button className="More">...</button>
-        <div className="SelectedMessage">
-          <button className="SelectedNumber">4</button>
-          <div className="Font"> items have been selected</div>
-          <button className="Clear">clear</button>
-        </div>
+      <div>
+        <AdminUsersSearch/>
         <AdminUsersTable/>
       </div>
     )
--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 import { login } from '../@api'
+import { AdminUsersTable } from '../components/admin-user-components/AdminUsersTable'
+import { AdminUsersSearch } from '../components/admin-user-components/AdminUsersSearch'
 
 export class TestPage extends Component {
+
 
   handleClick = async () => {
     await login({ email: '925993394@qq.com', password: '0' })
